322b68d083f505936c172185847a0cc6704823ea
naga4103 <naganaveena103@gmail.com>
2024-02-09 15:21:28 +0530

    Use Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Card, Button } from "react-bootstrap";
+
+import "./App.css";
+
+//use state is not the only thing to manage state. we also use useReducer which also allows to manage state and render component
+
+type CounterState = {
+  count: number;
+};
+
+type CounterAction = {
+  type: string;
+};
+
+function reducer(state: CounterState, action: CounterAction) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count };
+
+    case "decrement":
+      return { count: state.count };
+
+    default:
+      return state;
+  }
 }
+
+const initialState = {
+  count: 0,
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const [score, setScore] = useState(0);
+
+  // const onIncrement = () => {
+  //  setScore(score + 1);
+  // };
+
+  // const onDecrement = () => {
+  // setScore(score - 1);
+  // };
+  return (
+    <>
+      <Card>
+        <Card.Header>Counter</Card.Header>
+        <Card.Body>
+          <Button
+            variant="success"
+            onClick={() => {
+              dispatch({ type: "increment" });
+            }}
+          >
+            Increment
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => {
+              dispatch({ type: "decrement" });
+            }}
+          >
+            Decrement
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default App;
